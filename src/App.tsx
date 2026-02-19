479fb4b8c80021e874f1d18ed22996096216cac5
dvizhok <kalibry1@gmail.com>
2026-02-19 23:08:21 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from './components/cafeInfo/CafeInfo';
+import Notification from './components/notification/Notification';
+import VoteOptions from './components/voteOptions/VoteOptions';
+import VoteStats from './components/voteStats/VoteStats';
 import { type Votes, type VoteType } from './types/votes';
 
 export default function App() {
@@ -11,9 +14,9 @@ export default function App() {
   });
 
   function handleVote(type: VoteType) {
-    setVotes(prev => ({
-      ...prev,
-      [type]: prev[type] + 1,
+    setVotes(votes => ({
+      ...votes,
+      [type]: votes[type] + 1,
     }));
   }
 
@@ -24,11 +27,19 @@ export default function App() {
       bad: 0,
     });
   }
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
 
   return (
     <>
       <div className={css.app}>
         <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0 && true} />
+        {totalVotes > 0 ? (
+          <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />
+        ) : (
+          <Notification />
+        )}
       </div>
     </>
   );
